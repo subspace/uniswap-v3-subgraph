b81a7ed43de076df38238472df436bc38cea86cf
mzywang <mzywang@users.noreply.github.com>
2024-04-23 18:14:20 +0000

    fix(lint): auto-fix [ci]

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -11,11 +11,11 @@ const USDC_WETH_05_POOL = '0x4c36388be6f416a29c8d8eee81c771ce6be14b18'
 // usually tokens that many tokens are paired with s
 export const WHITELIST_TOKENS: string[] = [
   WETH_ADDRESS, // WETH
-  '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913' // USDC
+  '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // USDC
 ]
 
 const STABLE_COINS: string[] = [
-  '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913' // USDC
+  '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // USDC
 ]
 
 const MINIMUM_ETH_LOCKED = BigDecimal.fromString('1')
@@ -24,10 +24,7 @@ const Q192 = BigInt.fromI32(2).pow(192 as u8)
 export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, token1: Token): BigDecimal[] {
   const num = sqrtPriceX96.times(sqrtPriceX96).toBigDecimal()
   const denom = BigDecimal.fromString(Q192.toString())
-  const price1 = num
-    .div(denom)
-    .times(exponentToBigDecimal(token0.decimals))
-    .div(exponentToBigDecimal(token1.decimals))
+  const price1 = num.div(denom).times(exponentToBigDecimal(token0.decimals)).div(exponentToBigDecimal(token1.decimals))
 
   const price0 = safeDiv(BigDecimal.fromString('1'), price1)
   return [price0, price1]
