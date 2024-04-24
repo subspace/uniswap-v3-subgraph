d0a26ce3d521fdef05d079b5633ccc81b75e16d9
Michael Wang <44713145+mzywang@users.noreply.github.com>
2024-04-24 17:38:41 -0400

    update network to csv

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -4,38 +4,28 @@ import { exponentToBigDecimal, safeDiv } from '../utils/index'
 import { Bundle, Pool, Token } from './../types/schema'
 import { ONE_BD, ZERO_BD, ZERO_BI } from './constants'
 
-const WAVAX_ADDRESS = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7'
-const WAVAX_USDC_05_POOL = '0xfae3f424a0a47706811521e3ee268f00cfb5c45e'
+const WBNB_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+const USDC_WBNB_03_POOL = '0x6fe9e9de56356f7edbfcbb29fab7cd69471a4869'
 
-const DAI_E_ADDRESS = '0xd586e7f844cea2f87f50152665bcbc2c279d8d70'
-const DAI_ADDRESS = '0xba7deebbfc5fa1100fb055a87773e1e99cd3507a'
-const USDC_E_ADDRESS = '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664'
-const USDC_ADDRESS = '0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e'
-const USDT_E_ADDRESS = '0xc7198437980c041c805a1edcba50c1ce5db95118'
-const USDT_ADDRESS = '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7'
+const USDT_ADDRESS = '0x55d398326f99059ff775485246999027b3197955'
+const USDC_ADDRESS = '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
-export const WHITELIST_TOKENS: string[] = [
-  WAVAX_ADDRESS,
-  DAI_E_ADDRESS,
-  DAI_ADDRESS,
-  USDC_E_ADDRESS,
-  USDC_ADDRESS,
-  USDT_E_ADDRESS,
-  USDT_ADDRESS,
-  '0x130966628846bfd36ff31a822705796e8cb8c18d' // mim
-]
+export const WHITELIST_TOKENS: string[] = [WBNB_ADDRESS, USDT_ADDRESS, USDC_ADDRESS]
 
-const STABLE_COINS: string[] = [DAI_E_ADDRESS, DAI_ADDRESS, USDC_E_ADDRESS, USDC_ADDRESS, USDT_E_ADDRESS, USDT_ADDRESS]
+const STABLE_COINS: string[] = [USDT_ADDRESS, USDC_ADDRESS]
 
-const MINIMUM_ETH_LOCKED = BigDecimal.fromString('1000')
+const MINIMUM_ETH_LOCKED = BigDecimal.fromString('300')
 
 const Q192 = BigInt.fromI32(2).pow(192 as u8)
 export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, token1: Token): BigDecimal[] {
   const num = sqrtPriceX96.times(sqrtPriceX96).toBigDecimal()
   const denom = BigDecimal.fromString(Q192.toString())
-  const price1 = num.div(denom).times(exponentToBigDecimal(token0.decimals)).div(exponentToBigDecimal(token1.decimals))
+  const price1 = num
+    .div(denom)
+    .times(exponentToBigDecimal(token0.decimals))
+    .div(exponentToBigDecimal(token1.decimals))
 
   const price0 = safeDiv(BigDecimal.fromString('1'), price1)
   return [price0, price1]
@@ -43,9 +33,9 @@ export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, t
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
-  const usdcPool = Pool.load(WAVAX_USDC_05_POOL) // usdc is token1
+  const usdcPool = Pool.load(USDC_WBNB_03_POOL) // usdc is token0
   if (usdcPool !== null) {
-    return usdcPool.token1Price
+    return usdcPool.token0Price
   } else {
     return ZERO_BD
   }
@@ -56,7 +46,7 @@ export function getEthPriceInUSD(): BigDecimal {
  * @todo update to be derived ETH (add stablecoin estimates)
  **/
 export function findEthPerToken(token: Token): BigDecimal {
-  if (token.id == WAVAX_ADDRESS) {
+  if (token.id == WBNB_ADDRESS) {
     return ONE_BD
   }
   const whiteList = token.whitelistPools
